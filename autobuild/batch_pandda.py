87e990b373c1c663ac7f134d8d8af31cf06a3ef2
cwild <conor.wild@sky.com>
2021-04-06 12:11:12 +0100

    y

---
+++ autobuild/batch_pandda.py
@@ -0,0 +1,123 @@
+import os
+import subprocess
+from pathlib import Path
+
+import fire
+import pandas as pd
+
+from constants import Constants
+
+
+class Event:
+    def __init__(self):
+        self.x
+        self.y
+        self.z
+
+def execute(command: str):
+    p = subprocess.Popen(command,
+                         shell=True,
+                         )
+
+    p.communicate()
+
+
+def try_make_dir(path: Path):
+    if not path.exists():
+        os.mkdir(str(path))
+
+
+def chmod(path: Path):
+    p = subprocess.Popen(
+        f"chmod 777 {str(path)}",
+        shell=True,
+    )
+
+    p.communicate()
+
+
+def dispatch(event: Event, out_dir: Path):
+    event_id = f"{event.dtag}_{event.event_idx}"
+
+    print(f"Event id: {event_id}")
+
+    # Create the Event dir
+    event_dir = out_dir / event_id
+    try_make_dir(event_dir)
+
+    # Write the script that will call python to autobuild the event
+    model = event.model_path
+    xmap = event.event_map
+    mtz = event.reflections_path
+    smiles = event.smiles_path
+
+    executable_script = Constants.EXECUTABLE.format(model=model,
+                                                    xmap=xmap,
+                                                    mtz=mtz,
+                                                    smiles=smiles,
+                                                    x=event.x,
+                                                    y=event.y,
+                                                    z=event.z,
+                                                    out_dir=str(event_dir)
+                                                    )
+    executable_script_file = event_dir / Constants.EXECUTABLE_SCRIPT_FILE.format(dtag=event.dataset.dtag,
+                                                                                 event_idx=event.event_idx)
+    with open(executable_script_file, "w") as f:
+        f.write(executable_script)
+
+    chmod(executable_script_file)
+
+    # Generate a job script file for a condor cluster
+    executable_file = str(executable_script_file)
+    log_file = event_dir / Constants.LOG_FILE.format(event_id=event_id)
+    output_file = event_dir / Constants.OUTPUT_FILE.format(event_id=event_id)
+    error_file = event_dir / Constants.ERROR_FILE.format(event_id=event_id)
+    request_memory = Constants.REQUEST_MEMORY
+    job_script = Constants.JOB.format(
+        executable_file=executable_file,
+        log_file=log_file,
+        output_file=output_file,
+        error_file=error_file,
+        request_memory=request_memory,
+    )
+    job_script_file = event_dir / Constants.JOB_SCRIPT_FILE.format(dtag=event.dataset.dtag,
+                                                                   event_idx=event.event_idx)
+    with open(job_script_file, "w") as f:
+        f.write(job_script)
+
+    # Generate a shell command to submit the job to run the python script
+    command = Constants.COMMAND.format(job_script_file=job_script_file)
+    print(f"Command: {command}")
+
+    # Submit the job
+    execute(command)
+
+def get_event_list(pandda_event_table, pandda_dir):
+    for index, row in pandda_event_table.iterrows():
+        event = Event(
+
+        )
+
+def main(pandda_dir: str, output_dir: str):
+    # Format arguments
+    pandda_dir_path = Path(pandda_dir)
+    output_dir_path = Path(output_dir)
+
+    print(f"Database file path: {pandda_dir_path}")
+    print(f"Database file path: {output_dir_path}")
+
+    # Load database
+    pandda_event_table = pd.read_csv(str(pandda_dir_path / "analyses" / "pandda_analyse_events.csv"))
+
+    # Select which datasets to build
+    event_list = get_event_list(pandda_event_table, pandda_dir)
+    print(f"Got {len(event_list)} events")
+
+
+    for event in event_list:
+
+        dispatch(event, output_dir_path)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
