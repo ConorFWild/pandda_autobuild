d11e08080a5f3451eb3b7fd18e8e92a5126a0aa5
cwild <conor.wild@sky.com>
2021-04-20 16:49:29 +0100

    y

--- autobuild/autobuild.py
+++ autobuild/autobuild.py
@@ -133,7 +133,7 @@ def get_cut_out_event_map(event_map: gemmi.FloatGrid, coord: Coord, radius: floa
 
 def get_event_map(event_map_file: Path) -> gemmi.FloatGrid:
     m = gemmi.read_ccp4_map(str(event_map_file))
-    m.setup()
+    # m.setup()
 
     grid_array = np.array(m.grid, copy=True)
 
